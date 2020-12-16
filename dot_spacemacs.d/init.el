3d6281f6c738622c5d74606f6b43d290f544d29f
Anton Alekseev <anton@alekseev.cc>
2020-12-16 04:09:51 +0300

    Configure spaceline properly and early

--- dot_spacemacs.d/init.el
+++ dot_spacemacs.d/init.el
@@ -526,6 +526,7 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 
+  ;; Custom packages
   (use-package direnv
     :demand t
     :config
@@ -537,6 +538,10 @@ you should place your code here."
   (use-package evil-embrace
     :config
     (evil-embrace-enable-evil-surround-integration))
+
+  ;; Spaceline
+  (setq spaceline-hud-p nil
+        spaceline-minor-modes-p nil)
 
   ;; Don't show *spacemacs* buffer at startup
   ;; https://github.com/syl20bnr/spacemacs/issues/6899#issue-172716215
@@ -676,10 +681,6 @@ you should place your code here."
   ;; Disable spacemacs startup screen, switch to *scratch* by default
   (setq inhibit-startup-screen t)
   (setq initial-buffer-choice t)
-
-  ;; Spaceline
-  (spaceline-toggle-hud-off)
-  (spaceline-toggle-minor-modes-off)
 
   ;; yasnippet
   ;; =========
