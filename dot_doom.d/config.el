4066ba0bfc9018ecb0af0549d2e9c3b565d423b8
Anton Alekseev <anton@alekseev.cc>
2023-05-17 03:45:51 +0700

    Fix `browse-at-remote` for URLs starting with `gitlab.`
    Though `forge-commands` aren't working even with `(magit +forge)` due to
    https://github.com/doomemacs/doomemacs/issues/7191. It was fixed, but my
    Doom version predates the fix.

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -118,3 +118,8 @@
 ;; Suppress annoying "Warning: docstring has wrong usage of unescaped single quotes (use \= or different quoting)"
 ;; https://yhetil.org/emacs-devel/87pmjl2br2.fsf@posteo.net/
 (setq byte-compile-warnings (not 'docstrings))
+
+;; Set remote type to `gitlab` for URLs starting with `gitlab.`
+(after! browse-at-remote
+  (add-to-list 'browse-at-remote-remote-type-regexps
+               '("^gitlab\\..*" . "gitlab")))
