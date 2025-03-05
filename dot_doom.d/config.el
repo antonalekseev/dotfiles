041995e2c8bf37a2cf226446e7fb1359603cc292
Anton Alekseev <anton@alekseev.cc>
2025-03-06 00:40:51 +0700

    Remove package pins & workarounds
    that became irrelevant after the recent update of Doom Emacs.

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -125,16 +125,6 @@
   (add-to-list 'browse-at-remote-remote-type-regexps
                '("^gitlab\\..*" . "gitlab")))
 
-;; Re-enable `smerge-mode` that was disabled in
-;; https://github.com/doomemacs/doomemacs/commit/fe3f8866d81f32ef4edb2cd5be7214ad86b65447
-;; due to the upstream bug
-;; https://github.com/emacs-evil/evil-collection/pull/705 which is now resolved
-(after! evil-collection
-  (progn
-    (add-to-list 'evil-collection-mode-list 'smerge-mode)
-    (+evil-collection-init 'smerge-mode)
-    ) )
-
 ;; Use GNU grep on macOS for faster `consult-grep`
 ;; `doom doctor` is still complaining, because of hard-coded `grep` command
 (after! consult
--- dot_doom.d/packages.el
+++ dot_doom.d/packages.el
@@ -59,20 +59,9 @@
            :protocol ssh
            :repo "twlz0ne/whitespace4r.el"))
 
-;; Use the version that includes https://github.com/emacs-evil/evil-collection/pull/705/commits/30b5a2d79cd8526781ebe276223997b07645bef0
-(unpin! evil-collection)
-(package! evil-collection
-  :pin "0df5c4815d7be01ab52e9f957c56e6318c871217"
-  :recipe (:host github :repo "emacs-evil/evil-collection")
-  )
-
 ;; Disable solaire-mode to make scrolling and cursor movement less painful on macOS
 ;; see https://github.com/doomemacs/doomemacs/issues/2217 for the details
 (package! solaire-mode :disable t)
-
-;; Fix broken ts-ls
-;; https://github.com/doomemacs/doomemacs/issues/7540
-(unpin! lsp-mode)
 
 (package! annotate.el
   :recipe (:host github
