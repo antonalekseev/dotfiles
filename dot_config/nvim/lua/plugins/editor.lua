dd0478e5b56af4b92bf00cee3b69f39db953135e
Anton Alekseev <anton@alekseev.cc>
2025-03-13 15:45:38 +0700

    Pin nvim-surront to v3, update nvim-treesitter

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -1,7 +1,7 @@
 return {
   {
     "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
       require("nvim-surround").setup({
