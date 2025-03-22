0ef5cad1979e594ed8be85bd85ab477630db41c6
Anton Alekseev <anton@alekseev.cc>
2025-03-22 20:50:16 +0700

    Disable ledger, enable lua

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -147,8 +147,8 @@
        ;;kotlin            ; a better, slicker Java(Script)
        latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
-       ledger            ; be audit you can be
-       ;;lua               ; one-based indices? one-based indices
+       ;; ledger            ; be audit you can be
+       lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
