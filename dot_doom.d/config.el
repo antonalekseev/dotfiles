6da97ed1c53086e83bd2fd302a043c960f771b6d
Anton Alekseev <anton@alekseev.cc>
2025-07-06 01:09:28 +0700

    Improve font clarity
    - Set size to 16, for 15 leads to the odd spacing issues
    
    - `defaults write org.gnu.Emacs AppleFontSmoothing -int 0` fixed
    annoying thickness

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -23,9 +23,9 @@
 ;;
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-(setq doom-font (font-spec :family "PragmataPro Liga" :size 15 :weight 'normal)
-      doom-variable-pitch-font (font-spec :family "PragmataPro Liga" :size 15))
-;;
+
+(setq doom-font "PragmataPro-16")
+
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
