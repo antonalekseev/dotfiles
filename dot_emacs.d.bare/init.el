2c960f6e8add4a7b3d824cb19c68503e7f9195c0
Anton Alekseev <anton@alekseev.cc>
2025-07-12 20:42:11 +0700

    Add a bare bones Emacs config
    to run it as `emacs --init-dir ~/.emacs.d.bare`.

---
+++ dot_emacs.d.bare/init.el
@@ -0,0 +1,14 @@
+;;; init.el -*- lexical-binding: t; -*-
+
+(provide 'init)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(load-theme 'modus-operandi)
+
+(fido-vertical-mode)
+(setq make-backup-files nil)
+
+;;; init.el ends here
